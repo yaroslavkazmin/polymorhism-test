d9f7aae59f4ae20e765e2df3173ee9851c78c4d8
Yaroslav Kazmin <yaroslav.kazmin.25@kse.org.ua>
2026-03-05 21:45:06 +0200

    desperatly trying to make anything work, just a mandatory update

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -1,14 +1,29 @@
-﻿public class UniversityRegistry
+﻿public class uni_member
 {
+	private string name1;
+	public string memberid { get; }
+	protected List<string> ActionLog { get; } = new List<string>();
 
+	public string Name
+	{
+		get => name1;
+		set => name1 = value;
+	}
+}
+//public int count1 => countlog.Count;
+protected uni_member(string name, string id)
+{
+	Name = name;
+	memberid = id;
+}
+public class UniversityRegistry
+{
 	private List<uni_member> members = new List<uni_member>();
-
 	public void add_member(uni_member m)
 	{
 		members.Add(m);
 	}
-
-	public void execute()
+	public void PerformDuties()
 	{
 		foreach (var member in members)
 		{
@@ -33,4 +48,15 @@
 		return total_acts;
 	}
 }
+public class student : uni_member
+{
+	public student(string name, string id) : base(name, id) { }
+
+	public override void PerformDuties()
+	{
+		base.PerformDuties();
+		ActionLog.Add("лабораторка завершена");
+		Console.WriteLine($"{name} оновився");
+	}
 	
+}
