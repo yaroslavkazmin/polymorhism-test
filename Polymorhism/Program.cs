37cef39e4e4e70c4db40a33dc41915608c603397
Yaroslav Kazmin <yaroslav.kazmin.25@kse.org.ua>
2026-03-05 21:01:21 +0200

    probably fixed student registry, for now

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -1,11 +1,13 @@
 ﻿public class UniversityRegistry
 {
-	public static void Main()
-	{
+
 	private List<uni_member> members = new List<uni_member>();
+
 	public void add_member(uni_member m)
 	{
-		members.Add(m);}
+		members.Add(m);
+	}
+
 	public void execute()
 	{
 		foreach (var member in members)
@@ -23,10 +25,12 @@
 
 	public int member_stat()
 	{
-		int total_acts = 0
-			foreach(var member in members)
-			{}
-			return total_acts;
-	}
+		int total_acts = 0;
+		foreach (var member in members)
+		{
+		}
+
+		return total_acts;
 	}
 }
+	
