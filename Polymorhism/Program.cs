a6ca2013b6753127a9c8481a4f852fad353e38e4
Yaroslav Kazmin <yaroslav.kazmin.25@kse.org.ua>
2026-03-05 20:54:50 +0200

    kinda finished registry, but not sure if its working right. Gotta fix it 😔

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class UniversityRegistry
+{
+	public static void Main()
+	{
+	private List<uni_member> members = new List<uni_member>();
+	public void add_member(uni_member m)
+	{
+		members.Add(m);}
+	public void execute()
+	{
+		foreach (var member in members)
+		{
+			try
+			{
+				member.PerformDuties();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Помилка {member:Name}: {ex.Message}");
+			}
+		}
+	}
 
-Console.WriteLine("Hello, World!");
+	public int member_stat()
+	{
+		int total_acts = 0
+			foreach(var member in members)
+			{}
+			return total_acts;
+	}
+	}
+}
